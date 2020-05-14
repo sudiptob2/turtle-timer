1346ccc68a50313d1f33251311e64e27dcc667bd
sudipto baral <sudiptobaral.me@gmail.com>
2020-05-14 23:20:56 +0600

    UI design complete

--- src/App.js
+++ src/App.js
@@ -6,25 +6,31 @@ import Timer from "./components/Timer";
 
 function App() {
     return (
-        <div className="App ">
+        <div className="App">
             <div className="content-wrap">
-                <div className="row no-bottom jumbotron jumbotron-fluid justify-content-center">
+                <div className="row no-bottom jumbotron jumbotron-fluid justify-content-center ">
                     <div className="text-center">
                         <h1>Turtle Race </h1>
                         <h1> Count Down Timer</h1>
                     </div>
                 </div>
-                <div className="row ">
+                <div className="row no-gutters">
                     <div className="border col-md">
                         <Canvas />
                     </div>
                     <div className="border col-md">
                         <Timer />
                     </div>
-                    <div className="container-fluid">
-                        <FooterPage />
-                    </div>
                 </div>
+                <div className="row justify-content-center alert alert-light no-gutters">
+                    <p className="text-center">
+                        Choose a Turtle in your mind. When the timer finishes
+                        you can test your luck weather your turtle won or not.
+                        It's really fun!!
+                    </p>
+                </div>
+
+                <FooterPage />
             </div>
         </div>
     );
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,13 +1,41 @@
 import React from "react";
+import "./Timer.css";
 
 function Timer() {
     return (
-        <div>
-            <h1>I am the timer here</h1>
-            <button type="button" class="btn btn-primary">
-                Start Race!
-            </button>
-        </div>
+        <section className="timer container">
+            <div className="row justify-content-center alert alert-light no-gutters">
+                <div className="col col-3 text-center">
+                    <section>
+                        <h1>00</h1>
+                        <p>
+                            <small>Hours</small>
+                        </p>
+                    </section>
+                </div>
+
+                <div className="col col-3  text-center">
+                    <section>
+                        <h1>00</h1>
+                        <p>
+                            <small>Minitues</small>
+                        </p>
+                    </section>
+                </div>
+
+                <div className="col col-3  text-center">
+                    <section>
+                        <h1>00</h1>
+                        <p>
+                            <small>Sec</small>
+                        </p>
+                    </section>
+                </div>
+            </div>
+            <div className="row justify-content-center pb-3">
+                <button className="btn btn-outline-primary">Set Timer!</button>
+            </div>
+        </section>
     );
 }
 
