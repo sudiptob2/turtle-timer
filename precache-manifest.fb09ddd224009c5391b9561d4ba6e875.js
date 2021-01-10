e754bc13ea33303d50eaf68aa9a133763f22e0c9
sudipto baral <sudiptobaral.me@gmail.com>
2021-01-10 21:14:42 +0600

    Updates

---
+++ precache-manifest.fb09ddd224009c5391b9561d4ba6e875.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d77daca24a86d1f6929b5c0db92991",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7aee8aab7a0331f11b52",
+    "url": "./static/css/2.257f4af1.chunk.css"
+  },
+  {
+    "revision": "d6ea14cc83e0fa1686ed",
+    "url": "./static/css/main.437a2c4d.chunk.css"
+  },
+  {
+    "revision": "7aee8aab7a0331f11b52",
+    "url": "./static/js/2.45a33d59.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.45a33d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8889381bcb412cdae4a",
+    "url": "./static/js/3.6dd7aba7.chunk.js"
+  },
+  {
+    "revision": "196f31064c641ef86b22f34244b7ad03",
+    "url": "./static/js/3.6dd7aba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6ea14cc83e0fa1686ed",
+    "url": "./static/js/main.7f8310e1.chunk.js"
+  },
+  {
+    "revision": "292438a50445ec31eb53",
+    "url": "./static/js/runtime-main.84b671ad.js"
+  },
+  {
+    "revision": "bd212efd2dc25f255bf36701c47c766a",
+    "url": "./static/media/alert.bd212efd.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6a4044cde2376683ae1da5ac449856ae.js"
+  "./precache-manifest.fb09ddd224009c5391b9561d4ba6e875.js"
 );
 
 self.addEventListener('message', (event) => {
